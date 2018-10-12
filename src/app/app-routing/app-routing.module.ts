90b1a028f22442a06abfdca6d3adfb7d96abf621
purna <purna.penumalli@gmail.com>
2018-10-12 19:22:54 +0530

    commit

--- src/app/app-routing/app-routing.module.ts
+++ src/app/app-routing/app-routing.module.ts
@@ -2,8 +2,8 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {IndexComponent} from '../index/index.component';
 import {RouterModule,Routes} from '@angular/router';
-import {DisplayDataInTemplateComponent} from '../components-templates/display-data-in-template/display-data-in-template.component'
-import {DisplayDataInTemplateUrlComponent} from '../components-templates/display-data-in-template-url/display-data-in-template-url.component'
+import {DisplayDataInTemplateComponent} from '../components-templates/display-data-in-template/display-data-in-template.component';
+import {DisplayDataInTemplateUrlComponent} from '../components-templates/display-data-in-template-url/display-data-in-template-url.component';
 import {PageNotFoundComponent} from './page-not-found/page-not-found.component';
 import {TemplateSyntaxComponent} from '../components-templates/template-syntax/template-syntax.component';
 const appRouter:Routes=[
